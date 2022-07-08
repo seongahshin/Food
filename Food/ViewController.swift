8db2d7eba8c5699f68bd3afd1cb8d73ce8c6344f
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-07-08 09:41:25 +0900

    깃헙 커밋 테스트

--- Food/ViewController.swift
+++ Food/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var adImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func adButton(_ sender: UIButton) {
+        adImageView.image = UIImage(named: "banner\(Int.random(in:1...3))")
+    }
+    
 }
 
